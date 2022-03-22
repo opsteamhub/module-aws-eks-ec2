7e2b8a47027f4f9c7474411d61b0664c7f842cb3
rafael <rafaelviniciuscardoso@gmail.com>
2022-03-22 11:04:13 -0300

    update security group egress

--- security_group.tf
+++ security_group.tf
@@ -38,11 +38,13 @@ resource "aws_security_group" "node_group_sg" {
 }
 
   egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
+
 
   tags = {
     "Name"      = join("-", ["SG", local.name])
