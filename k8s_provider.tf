b5864a8c006829b877addecf795f3429b0f79e7e
rafael <rafaelviniciuscardoso@gmail.com>
2022-03-22 14:40:27 -0300

    update provider kubernetes

--- k8s_provider.tf
+++ k8s_provider.tf
@@ -1,5 +1,6 @@
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster_endpoint.endpoint
+  #host                   = data.aws_eks_cluster.cluster_endpoint.endpoint
+  host = "https://EE01BA6999346DD665B6E8B2B0E28457.sk1.sa-east-1.eks.amazonaws.com"
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
